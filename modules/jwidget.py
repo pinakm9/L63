dfa30b35ad3933a25d0fda8c23dc31bf0bb9135c
pinakm9 <pinakm9@gmail.com>
2021-08-02 01:45:24 +0530

    l2 error in widgets

--- modules/jwidget.py
+++ modules/jwidget.py
@@ -24,25 +24,31 @@ class AsmlWidget:
         self.data = tables.open_file(self.asml_file_path, 'r')
         self.observation = np.array(self.data.root.observation.read().tolist())
         self.num_steps = len(self.observation)
+        self.l2_err = np.zeros(self.num_steps)
         if hidden_file_path is not None:
             self.hidden_path = self.get_hidden_path()
-            print(self.hidden_path.shape)
         if attractor_path is not None:
             self.attractor = self.get_attractor()
             if len(self.attractor) > max_attractor_pts:
                 self.attractor = self.attractor[np.random.choice(len(self.attractor), size=max_attractor_pts, replace=False)]
-        self.asml_step_slider = widgets.IntSlider(value=0, min=0, max=self.num_steps, step=1)
+        self.asml_step_slider = widgets.IntSlider(value=0, min=0, max=self.num_steps-1, step=1)
         widgets.interact(self.ensemble_plot, step=self.asml_step_slider)
+        
 
 
     def ensemble_plot(self, step):
-        fig = plt.figure(figsize=(10, 10))
-        ax = fig.add_subplot(111)
+        fig = plt.figure(figsize=(10, 5))
+        ax = fig.add_subplot(121)
         posterior = self.get_ensemble(step)
         if self.attractor_path is not None:
             ax.scatter(self.attractor[:, self.dims[0]], self.attractor[:, self.dims[1]], c='green', s=100)
         ax.scatter(posterior[:, self.dims[0]], posterior[:, self.dims[1]], c='blue')
         ax.scatter(self.hidden_path[step, self.dims[0]], self.hidden_path[step, self.dims[1]], c='pink')
+        self.l2_err[step] = np.linalg.norm(np.mean(posterior, axis=0) - self.hidden_path[step])
+        ax_l2 = fig.add_subplot(122)
+        ax_l2.set_title('L2 error = {:.2f}'.format(self.l2_err[step]))
+        ax_l2.plot(range(self.num_steps), self.l2_err)
+        ax_l2.scatter(step, self.l2_err[step], s=30, c='red')
 
     def get_ensemble(self, step):
         return np.array(getattr(self.data.root.particles, 'time_' + str(step)).read().tolist())
